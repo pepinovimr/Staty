29a598f29007f976c08769d43fc172003755c12e
pepinovimr <pepino.vimr@seznam.cz>
2022-04-01 22:02:59 +0200

    While ordered, second use of the same order orders the list from Ascending to Descending & vice versa

--- Staty/Utils/Orderers.cs
+++ Staty/Utils/Orderers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Staty.Data;
+using System.Linq;
 namespace Staty.Utils
 {
     //Třída na řazení elementů v Listu
@@ -7,21 +8,26 @@ namespace Staty.Utils
     { 
         public static void NameOrder()
         {
-            
-            ReadStates.CurrentlyShownStates.Sort((x, y) => x.Name.CompareTo(y.Name));  //Dokáže rozpoznat a zařadit i Ch
-            //ReadStates.StatesList.Sort((x, y) => y.Name.CompareTo(x.Name));    //seřadí sestupně
+            if (!ReadStates.CurrentlyShownStates.SequenceEqual(ReadStates.CurrentlyShownStates.OrderBy(x => x.Name))) //Zjistí, jestli je List seřazen, pokud ne, seřadí ho vzestupně, pokud ano, seřadí ho sestupně
+                ReadStates.CurrentlyShownStates.Sort((x, y) => x.Name.CompareTo(y.Name));  //Dokáže rozpoznat a zařadit i Ch
+            else
+                ReadStates.CurrentlyShownStates.Sort((x, y) => y.Name.CompareTo(x.Name));    //seřadí sestupně
         }
 
         public static void AreaOrder()
         {
-            ReadStates.CurrentlyShownStates.Sort((x, y) => x.Area.CompareTo(y.Area));    //Sort setřídí List podle daného komparátoru, zde je v pohodě využít lambdu, protože je to jednoduchý porovnání
-            //ReadStates.StatesList.Sort((x, y) => y.Area.CompareTo(x.Area));  //Seřadí sestupně
+            if (!ReadStates.CurrentlyShownStates.SequenceEqual(ReadStates.CurrentlyShownStates.OrderBy(x => x.Area)))   //SequenceEqual zjišťuje
+                ReadStates.CurrentlyShownStates.Sort((x, y) => x.Area.CompareTo(y.Area));    //Sort setřídí List podle daného komparátoru, zde je v pohodě využít lambdu, protože je to jednoduchý porovnání
+            else
+                ReadStates.CurrentlyShownStates.Sort((x, y) => y.Area.CompareTo(x.Area));  //Seřadí sestupně
         }
 
         public static void PopulationOrder()
         {
-            ReadStates.CurrentlyShownStates.Sort((x, y) => x.Population.CompareTo(y.Population));
-            //ReadStates.StatesList.Sort((x, y) => y.Population.CompareTo(x.Population));  //Seřadí sestupně
+            if (!ReadStates.CurrentlyShownStates.SequenceEqual(ReadStates.CurrentlyShownStates.OrderBy(x => x.Population))) 
+                ReadStates.CurrentlyShownStates.Sort((x, y) => x.Population.CompareTo(y.Population));
+            else
+                ReadStates.CurrentlyShownStates.Sort((x, y) => y.Population.CompareTo(x.Population));  //Seřadí sestupně
         }
     }
 }
